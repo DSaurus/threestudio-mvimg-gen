dd0da8d69ea66a947a2677912a1ae6d0c0f2a732
DSaurus <2238454358@qq.com>
2023-12-19 20:09:06 +0800

    add version

--- __init__.py
+++ __init__.py
@@ -1,2 +1,16 @@
+import threestudio
+from packaging.version import Version
+
+if hasattr(threestudio, "__version__") and Version(threestudio.__version__) >= Version(
+    "0.2.0"
+):
+    pass
+else:
+    if hasattr(threestudio, "__version__"):
+        print(f"[INFO] threestudio version: {threestudio.__version__}")
+    raise ValueError(
+        "threestudio version must be >= 0.2.0, please update threestudio by pulling the latest version from github"
+    )
+
 from .guidance import stable_zero123_guidance
 from .system import stable_zero123_system
